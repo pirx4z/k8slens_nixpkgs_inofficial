83665ab532c7e61733ad27554e8c0c2caeef7929
benjamin.bajorat <benjamin.bajorat@mayflower.de>
2020-10-02 12:20:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hugh! Ok! Lens-3.6.5.AppImage package for nixos.
+###### Download that !@#*$ AppImage
+###### Wrap it and pack it (https://nixos.org/manual/nixpkgs/stable/#ssec-pkgs-appimageTools-wrapping)
+###### nix-prefetch-url 
+###### nix-build k8slens.nix
+###### ./result/bin/k8slens
+###### follow instructions
---
+++ k8slens.nix
@@ -0,0 +1,10 @@
+with import <nixpkgs> {};
+
+appimageTools.wrapType2 {
+    name = "k8slens"; 
+    src = fetchurl { 
+        url =  https://github.com/lensapp/lens/releases/download/v3.6.5/Lens-3.6.5.AppImage;
+        sha256 = "0vg71swk7xqgqjf4mna9ikzly8dplklyrpi61iav673frss54bb6";
+    };
+    extraPkgs = pkgs: with pkgs; [ ]; 
+}   
